a702a077e544a15d8a92edaf97c8eb2958be293e
jessusgarciar <l221050153@pabellon.tecnm.mx>
2023-10-09 20:14:50 -0600

    Act INE

---
+++ LISTAS_U2/listas.R
@@ -0,0 +1,32 @@
+> ITIC3 <- list("Santi Herrera", "Axel OM", "Juan Prieto");
+> Edades <- list(18,19,21);
+> Crreras <- list("ITIC", "Logistica", "Gestion", "Mecatronica", "Industrial");
+> EdadesAlumnos <- list(ITIC3, Edades);
+> View(EdadesAlumnos)
+> View(Edades)
+> ITIC3[3];
+> EdadesAlumnos[1];
+> EdadesAlumnos[[1]][[1]];
+> View(Edades)
+> length(Edades)
+> length(EdadesAlumnos)
+> Edades[4] <- ("31");
+> Edades <- ("36");
+> Edades
+> Edades <- list(18,19,21,30);
+> Edades[5] <- list(25);
+> View(Edades)
+> View(Edades)
+> Edades
+> Edades[4] <- NULL;
+> View(Edades)
+> Edades
+> ITIC3[5] <- list("Sajith Montaño")
+> View(ITIC3)
+> ITIC3
+> ITIC3[1] <- NULL
+> View(ITIC3)
+> ITIC3
+> Edades2 <- list(17,20,21);
+> View(Edades2)
+> Edades2
---
+++ PROGRAMAS/R/Unidad2/act_09_10.R
@@ -0,0 +1,12 @@
+library(readr)
+localidades2 <- read.csv("Users\User\Desktop\estrucdatos\PROGRAMAS\R\Unidad2\csv\cat_localidad.csv")
+View(localidades2)
+library(readr)
+municipios1 <- read.csv("Users\User\Desktop\estrucdatos\PROGRAMAS\R\Unidad2\csv\cat_municipio.csv")
+View(municipios1)
+length(localidades2)
+length(municipios1)
+municipios1[1] 
+localidades2[1] 
+lengths(municipios1)
+lengths(localidades2)
